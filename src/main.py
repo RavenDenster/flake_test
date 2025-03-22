9406e483142834f687139cca807764695e3a960d
RavenDenster <snaff2588@mail.ru>
2025-03-22 20:10:57 +0300

    new

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ print("hello")
 # ![Flake8 Status](https://github.com/moevm/qemu-riscv-cluster/workflows/
 # Flake8%20check%20(style%20check)/badge.svg?
 # branch=main)
-print("fff  ")
+print("fff   ")
